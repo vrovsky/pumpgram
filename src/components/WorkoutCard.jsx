9f5812aa50bc21bcf6d0b67ec7f7cc0dc8287b24
Viachaslau Rouski <vrovsky@gmail.com>
2025-05-21 12:09:57 +0100

    workoutcard-wip

--- src/components/WorkoutCard.jsx
+++ src/components/WorkoutCard.jsx
@@ -3,5 +3,14 @@ import React from "react";
 export default function WorkoutCard(props) {
   const { trainingPlan, workoutIndex, type } = props;
   const { warmup, workout } = trainingPlan || {};
-  return <div></div>;
+  return (
+    <div className="workout-container">
+      <div className="workout-card card">
+        <div className="plancard-header">
+          <p>Day {dayNum}</p>
+          {icon}
+        </div>
+      </div>
+    </div>
+  );
 }
